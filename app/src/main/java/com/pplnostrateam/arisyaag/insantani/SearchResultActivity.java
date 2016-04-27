6ea6405bbe446570e6b1814540203cf7b9e65b13
suciayu <sucilestri@gmail.com>
2016-04-27 16:50:33 +0700

    display search result

--- app/src/main/java/com/pplnostrateam/arisyaag/insantani/SearchResultActivity.java
+++ app/src/main/java/com/pplnostrateam/arisyaag/insantani/SearchResultActivity.java
@@ -1,15 +1,23 @@
 package com.pplnostrateam.arisyaag.insantani;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,29 +26,88 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class SearchingActivity extends AppCompatActivity {
-    //private EditText editTextName;
-    private EditText search_vegetable;
-    private Button button;
-    private TextView textView2;
-    private Activity activity;
+
+/**
+ * Created by Suci Ayu on 4/14/2016.
+ */
+
+
+public class SearchResultActivity extends AppCompatActivity {
     String json_string;
-    // private EditText editText;
-    @Override
+    JSONObject jsonObject;
+    JSONArray jsonArray;
+    VegetableAdapter vegetableAdapter;
+    EditText search_vegetable;
+
+
+    //    ArrayList<Vegetable> arrayOfData = new ArrayList<Vegetable>();
+//    FancyAdapter aa = null;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_searching);
-        //    textView = (TextView) findViewById(R.id.textView);
+        setContentView(R.layout.activity_search_result);
+        //TextView textView3 = (TextView) findViewById(R.id.textView3);
+        // ListView searchResult = (ListView)findViewById(R.id.searchResult);
+        ListView searchResult = (ListView)findViewById(R.id.searchResult);
+        vegetableAdapter = new VegetableAdapter(this, R.layout.row_layout);
+        searchResult.setAdapter(vegetableAdapter);
         search_vegetable = (EditText) findViewById(R.id.search_vegetable);
 
+        json_string = getIntent().getExtras().getString("json_data");
+        try {
+            // jsonObject = new JSONObject(json_string);
+            jsonArray = new JSONArray(json_string);
+            int count = 0;
+            String name;
+            int price, stock;
+            while(count<jsonArray.length()){
+                JSONObject JO = jsonArray.getJSONObject(count);
+                name = JO.getString("name");
+                price = JO.getInt("price");
+                stock = JO.getInt("stock");
+                Vegetable vegetable = new Vegetable(name, price, stock);
+                vegetableAdapter.add(vegetable);
+                count++;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
+    public void getConfirmation(View view){
+        //Intent intent = new Intent(this, SearchResultActivity.class);
+        AlertDialog.Builder alertDialogue = new AlertDialog.Builder(this);
+        alertDialogue.setTitle("You have to sign in first");
+        alertDialogue.setMessage("Are you sure you want to continue this order?");
+        alertDialogue.setCancelable(false);
+
+        alertDialogue.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                move();
+            }
+        });
+        alertDialogue.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog = alertDialogue.create();
+        dialog.show();
+    }
+
+    private void move() {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
     public void getData(View view){
         new BackgroundTask().execute();
 
@@ -92,14 +159,14 @@ public class SearchingActivity extends AppCompatActivity {
             //textView2 = (TextView) findViewById(R.id.textView2);
             //textView2.setText(result);
             json_string = result;
-            move();
+            move2();
         }
     }
-    public void move(){
+
+    public void move2(){
         Intent intent = new Intent(this, SearchResultActivity.class);
         intent.putExtra("json_data", json_string);
         startActivity(intent);
     }
-
 
 }
