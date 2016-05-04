90526d82c8c778b57404ec41b8ea61a2936b7e0c
suciayu <sucilestri@gmail.com>
2016-05-04 14:46:27 +0700

    membuat tombol enter pada keyboard berfungsi sebagai tombol search

--- app/src/main/java/com/pplnostrateam/arisyaag/insantani/SearchingActivity.java
+++ app/src/main/java/com/pplnostrateam/arisyaag/insantani/SearchingActivity.java
@@ -1,11 +1,15 @@
 package com.pplnostrateam.arisyaag.insantani;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,10 +31,12 @@ import okhttp3.Response;
 public class SearchingActivity extends AppCompatActivity {
     //private EditText editTextName;
     private EditText search_vegetable;
+    //private EditText editText;
     private Button button;
     private TextView textView2;
     private Activity activity;
     String json_string;
+    String vName;
     // private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +44,23 @@ public class SearchingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_searching);
         //    textView = (TextView) findViewById(R.id.textView);
         search_vegetable = (EditText) findViewById(R.id.search_vegetable);
+        //editText = (EditText) findViewById(R.id.editText);
+        vName = search_vegetable.getText().toString();
 
+        search_vegetable.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction (TextView v,int actionId, KeyEvent event){
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    getData();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
-    public void getData(View view){
+
+    public void getData(){
         new BackgroundTask().execute();
 
     }
@@ -49,7 +68,6 @@ public class SearchingActivity extends AppCompatActivity {
     class BackgroundTask extends AsyncTask<Void, Void, String> {
         String JSON_STRING;
         String data_url;
-        String vName = search_vegetable.getText().toString();
         private Activity activity;
 
         @Override
@@ -96,10 +114,26 @@ public class SearchingActivity extends AppCompatActivity {
         }
     }
     public void move(){
+//        if(vName == ""){
+//            AlertDialog.Builder alertDialogue = new AlertDialog.Builder(this);
+//            alertDialogue.setMessage("masukkan sayur yang ingin dicari");
+//            alertDialogue.setCancelable(false);
+//
+//            alertDialogue.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                }
+//            });
+//            AlertDialog dialog = alertDialogue.create();
+//            dialog.show();
+//        }
+//        else {
+//            Intent intent = new Intent(this, SearchResultActivity.class);
+//            intent.putExtra("json_data", json_string);
+//            startActivity(intent);
+//        }
         Intent intent = new Intent(this, SearchResultActivity.class);
         intent.putExtra("json_data", json_string);
         startActivity(intent);
     }
-
-
 }
