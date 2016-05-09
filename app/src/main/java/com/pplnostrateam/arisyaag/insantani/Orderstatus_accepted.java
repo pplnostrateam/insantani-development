c64d7a155cbbac99b6cd40d544edace10748d90d
ratnamukti <desi.rmu@gmail.com>
2016-05-09 12:28:43 +0700

    add first status order layout, edit ordermade button, and fix fragment background

---
+++ app/src/main/java/com/pplnostrateam/arisyaag/insantani/Orderstatus_accepted.java
@@ -0,0 +1,13 @@
+package com.pplnostrateam.arisyaag.insantani;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Orderstatus_accepted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orderstatus_accepted);
+    }
+}
